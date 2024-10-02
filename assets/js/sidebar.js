0a73b1616250ba0998cc478976d48969bb3811b7
daniela-sc <daniela148sc@gmail.com>
2024-10-02 17:10:39 -0500

    Realizo ajustes de traduccion de pagina

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -10,3 +10,38 @@ $(document).ready(function () {
 
     //$('#sidebar').load('../../components/sidebar.html');
 });
+
+
+// Configuramos el idioma
+window.addEventListener("DOMContentLoaded", () => {
+    // Función para cargar el archivo de idioma
+    function loadLanguage(lang) {
+        console.log(`Cargando idioma: ${lang}`);
+        fetch(`../../assets/${lang}.json`)
+            .then((response) => response.json())
+            .then((data) => {
+                document.querySelectorAll("[data-translate]").forEach((element) => {
+                    const translateKey = element.getAttribute("data-translate");
+                    if (data[translateKey]) {
+                        element.textContent = data[translateKey]; // Reemplazar el contenido
+                    }
+                });
+            })
+            .catch((error) =>
+                console.error("Error al cargar el archivo de idioma:", error)
+            );
+    }
+
+    // Detectar el clic en el selector de idioma y cambiar el idioma
+    document.querySelectorAll(".menu-item").forEach((item) => {
+        item.addEventListener("click", function (event) {
+            const selectedLang = item.getAttribute("data-lang");
+            console.log(`Idioma seleccionado: ${selectedLang}`);
+            document.querySelector(".selected-lang").textContent = item.textContent;
+            loadLanguage(selectedLang);
+        });
+    });
+});
+
+
+//Dar formato al documento y llamar los json.
